3fa63bf58ae55ebbc06aed1c6afaa4f4fe2d7055
Croodo <67230713+Croodo@users.noreply.github.com>
2024-03-29 23:36:39 +0530

    bio section

--- components/Quote.vue
+++ components/Quote.vue
@@ -1,14 +1,44 @@
 <template>
   <div class="bg-white relative z-10">
-    <div
-      class="max-w-6xl m-auto bg-[#f3fbf5] rounded-3xl px-20 py-10 text-lg shadow-inner"
-    >
-      I'm Bhaskar Barma, a 29-year-old boy from West Bengal, India. I'm
-      developing websites since 2010 as an individual, to run my own startup
-      concepts. Previously successfully ran a third-party Android app store and
-      some other small profitable websites. Know the importance of web UI/UX and
-      other technical fundamentals (SEO optimized, Valid Code, PageSpeed, etc.)
-      when it comes to developing a production-ready website.
+    <div class="px-6 sm:px-12 max-w-7xl m-auto">
+      <div class="grid grid-cols-2">
+        <div
+          class="rounded-3xl flex items-center justify-center relative h-full w-full overflow-hidden py-16"
+        >
+          <span
+            class="bg-green-200 h-full w-1/2 absolute top-0 left-0 -rotate-12 scale-150"
+          ></span>
+          <img
+            src="/img/bhaskar_barma.jpg"
+            class="w-1/2 aspect-square object-cover rounded-full ring-4 border-green-400 border-4 ring-white relative"
+          />
+        </div>
+        <div class="flex flex-col justify-center gap-3">
+          <div class="flex flex-col">
+            <span class="font-medium text-2xl italic">Name:</span>
+            <span class="italic text-zinc-600">Bhaskar Barma</span>
+          </div>
+          <div class="flex flex-col">
+            <span class="font-medium text-2xl italic">Age:</span>
+            <span class="italic text-zinc-600">29 Years</span>
+          </div>
+          <div class="flex flex-col">
+            <span class="font-medium text-2xl italic">Live in:</span>
+            <span class="italic text-zinc-600"
+              >Koch Bihar, West Bengal, India</span
+            >
+          </div>
+          <div class="flex flex-col">
+            <span class="font-medium text-2xl italic">Note:</span>
+            <span class="italic text-zinc-600"
+              >Developing websites for a long time, know the importance of good
+              web UI/UX and other technical fundamentals (SEO optimized, Valid
+              Code, PageSpeed, etc.) when it comes to developing a
+              production-ready website.</span
+            >
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
